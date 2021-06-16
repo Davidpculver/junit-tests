7d047272a906875afd019311b3844a425b0f9983
David Culver <culver.david.p@gmail.com>
2021-06-16 10:28:15 -0500

    walkthrough

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,14 +2,51 @@ import java.util.ArrayList;
 
 public class Student {
 
-    public static long id;
-    public static String name;
-    public static ArrayList<Integer> grades;
+//    private static long id;
+//    private static String name;
+//    private static ArrayList<Integer> grades;
 
-    public Student(long id, String name, ArrayList<Integer> grades) {
+
+//    public Student(long id, String name, ArrayList<Integer> grades) {
+//        this.id = id;
+//        this.name = name;
+//        this.grades = new ArrayList<>();
+//    }
+//
+//    public static long getId() {
+//        return id;
+//    }
+//
+//    public static String getName() {
+//        return name;
+//    }
+//
+//    public static ArrayList<Integer> getGrades() {
+//        return grades;
+//    }
+//
+//    public static void setId(long id) {
+//        Student.id = id;
+//    }
+//
+//    public static void setName(String name) {
+//        Student.name = name;
+//    }
+//
+//    public static void setGrades(ArrayList<Integer> grades) {
+//        Student.grades = grades;
+//    }
+
+
+//    instructor example
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
         this.id = id;
         this.name = name;
-        this.grades = grades;
+        this.grades = new ArrayList<>();
     }
 
     public long getId() {
@@ -23,4 +60,18 @@ public class Student {
     public ArrayList<Integer> getGrades() {
         return grades;
     }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double sum = 0;
+        for (int grade : grades){
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+
+
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,32 +1,99 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
+
 
 public class StudentTest {
 
 
+//    @Test
+//    public void testForStudentId(){
+////        Student student1 = new Student();
+//        assertEquals(0, Student.getId(), 0);
+//        Student.setId(125);
+//        assertEquals(125, Student.getId(), 0);
+//    }
 
-    @Test
-    public void testForStudentId(){
-//        Student student1 = new Student();
-        assertEquals(0, Student.id, 0);
-        Student.id = 125;
-        assertEquals(125, Student.id, 0);
-    }
-
-    @Test
-    public void testForStudentName(){
-        assertNull(null, Student.name);
-        Student.name = "David";
-        assertEquals("David", Student.name);
-    }
 //
-    @Test
-    public void testForGrades(){
-        assertNull(null, Student.grades.get(0));
-        grades.add(85);
-        assertEquals(1, grades.size());
+//    @Test
+//    public void testForStudentName(){
+//        assertNull(null, Student.getName());
+//        Student.setName("David");
+//        assertEquals("David", Student.getName());
+//    }
+////
+//    @Test
+//    public void testForGrades(){
+//        ArrayList<Integer> grades = new ArrayList<>();
+//        Student student = new Student(0, null, null);
+//        grades.add(85);
+//        assertEquals(1, grades.size());
+//    }
+
+    //    Instructor example
+    private Student stud;
+
+    @Before
+    public void setUp() {
+        this.stud = new Student(1, "David C");
     }
 
+    @Test
+    public void testStudentConstructor() {
 
+        assertEquals(1, stud.getId());
+        assertEquals("David C", stud.getName());
+        assertTrue(stud.getGrades().isEmpty());
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(1, stud.getId());
+        Student craig = new Student(27, "Creepy Craig");
+        assertEquals(27, craig.getId());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("David C", stud.getName());
+        Student bob = new Student(125, "Bob Bobberson");
+        assertEquals("Bob Bobberson", bob.getName());
+    }
+
+    @Test
+    public void addGrade() {
+        stud.addGrade(85);
+        stud.addGrade(72);
+        stud.addGrade(91);
+
+        assertEquals(3, stud.getGrades().size());
+        stud.addGrade(100);
+        assertEquals(4, stud.getGrades().size());
+        Student bob = new Student(125, "Bob Bobberson");
+        bob.addGrade(95);
+        assertEquals(1, bob.getGrades().size());
+    }
+
+    @Test
+    public void getGrades(){
+//        the changes made inside of addGrade test does not affect what is inside of getGrades - like block scope
+        assertEquals(0, stud.getGrades().size());
+        stud.addGrade(55);
+        assertEquals(1, stud.getGrades().size());
+    }
+
+    @Test
+    public void testGetGradeAverage(){
+        stud.addGrade(75);
+        stud.addGrade(25);
+        stud.addGrade(50);
+
+        assertEquals(50, stud.getGradeAverage(), 0);
+        stud.addGrade(10);
+        assertEquals(40, stud.getGradeAverage(), 0);
+    }
 
 }
