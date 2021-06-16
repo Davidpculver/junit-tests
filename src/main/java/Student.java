7076177356a33b25c8dbffbea575b1b4e7c72146
David Culver <culver.david.p@gmail.com>
2021-06-16 09:20:59 -0500

    working on students test

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,10 +2,25 @@ import java.util.ArrayList;
 
 public class Student {
 
-    public long id;
-    public String name;
-    public ArrayList<Integer> grades;
+    public static long id;
+    public static String name;
+    public static ArrayList<Integer> grades;
 
+    public Student(long id, String name, ArrayList<Integer> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
 
+    public long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -3,8 +3,11 @@ import static org.junit.Assert.*;
 
 public class StudentTest {
 
+
+
     @Test
     public void testForStudentId(){
+//        Student student1 = new Student();
         assertEquals(0, Student.id, 0);
         Student.id = 125;
         assertEquals(125, Student.id, 0);
@@ -12,14 +15,14 @@ public class StudentTest {
 
     @Test
     public void testForStudentName(){
-        assertEquals(null, Student.name);
+        assertNull(null, Student.name);
         Student.name = "David";
         assertEquals("David", Student.name);
     }
-
+//
     @Test
     public void testForGrades(){
-        assertEquals(null, Student.grades.get(0));
+        assertNull(null, Student.grades.get(0));
         grades.add(85);
         assertEquals(1, grades.size());
     }
